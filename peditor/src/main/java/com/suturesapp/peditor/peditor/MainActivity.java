b065d3a27ccf3c78c6300827f5e81faee7ec94e2
Achraf <achraf.amil@gmail.com>
2017-03-04 13:25:39 +0000

    Added PEditor module.
    Details: still need to integrate the two modules.

---
+++ peditor/src/main/java/com/suturesapp/peditor/peditor/MainActivity.java
@@ -0,0 +1,212 @@
+package com.suturesapp.peditor.peditor;
+
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.Environment;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoadImage, btnSaveImage;
+    ImageView imageResult;
+
+    final int RQS_IMAGE1 = 1;
+
+    Uri source;
+    Bitmap bitmapMaster;
+    Canvas canvasMaster;
+
+    int prvX, prvY;
+
+    Paint paintDraw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoadImage = (Button)findViewById(R.id.loadimage);
+        btnSaveImage = (Button)findViewById(R.id.saveimage);
+        imageResult = (ImageView)findViewById(R.id.result);
+
+        paintDraw = new Paint();
+        paintDraw.setStyle(Paint.Style.FILL);
+        paintDraw.setColor(Color.BLACK);
+        paintDraw.setStrokeWidth(10);
+
+        btnLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RQS_IMAGE1);
+            }
+        });
+
+
+        btnSaveImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(bitmapMaster != null){
+
+                    saveBitmap(bitmapMaster);
+                }
+            }
+        });
+
+        imageResult.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                int action = event.getAction();
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        prvX = x;
+                        prvY = y;
+                        drawOnProjectedBitMap((ImageView) v, bitmapMaster, prvX, prvY, x, y);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        drawOnProjectedBitMap((ImageView) v, bitmapMaster, prvX, prvY, x, y);
+                        prvX = x;
+                        prvY = y;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        drawOnProjectedBitMap((ImageView) v, bitmapMaster, prvX, prvY, x, y);
+                        break;
+                }
+    /*
+     * Return 'true' to indicate that the event have been consumed.
+     * If auto-generated 'false', your code can detect ACTION_DOWN only,
+     * cannot detect ACTION_MOVE and ACTION_UP.
+     */
+                return true;
+            }
+        });
+    }
+
+
+    /*
+    Project position on ImageView to position on Bitmap draw on it
+     */
+
+    private void drawOnProjectedBitMap(ImageView iv, Bitmap bm,
+                                       float x0, float y0, float x, float y){
+        if(x<0 || y<0 || x > iv.getWidth() || y > iv.getHeight()){
+            //outside ImageView
+            return;
+        }else{
+
+            float ratioWidth = (float)bm.getWidth()/(float)iv.getWidth();
+            float ratioHeight = (float)bm.getHeight()/(float)iv.getHeight();
+
+            canvasMaster.drawLine(
+                    x0 * ratioWidth,
+                    y0 * ratioHeight,
+                    x * ratioWidth,
+                    y * ratioHeight,
+                    paintDraw);
+            imageResult.invalidate();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap tempBitmap;
+
+        if(resultCode == RESULT_OK){
+            switch (requestCode){
+                case RQS_IMAGE1:
+                    source = data.getData();
+
+                    try {
+                        //tempBitmap is Immutable bitmap,
+                        //cannot be passed to Canvas constructor
+                        tempBitmap = BitmapFactory.decodeStream(
+                                getContentResolver().openInputStream(source));
+
+                        Bitmap.Config config;
+                        if(tempBitmap.getConfig() != null){
+                            config = tempBitmap.getConfig();
+                        }else{
+                            config = Bitmap.Config.ARGB_8888;
+                        }
+
+                        //bitmapMaster is Mutable bitmap
+                        bitmapMaster = Bitmap.createBitmap(
+                                tempBitmap.getWidth(),
+                                tempBitmap.getHeight(),
+                                config);
+
+                        canvasMaster = new Canvas(bitmapMaster);
+                        canvasMaster.drawBitmap(tempBitmap, 0, 0, null);
+
+                        imageResult.setImageBitmap(bitmapMaster);
+                    } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    break;
+            }
+        }
+    }
+
+
+    private void saveBitmap(Bitmap bm){
+        File file = Environment.getExternalStorageDirectory();
+        File newFile = new File(file, "test.jpg");
+
+
+
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(newFile);
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            //byWalid
+            MediaStore.Images.Media.insertImage(getContentResolver() , bm , "TitleName" , "") ;
+            Toast.makeText(MainActivity.this,
+                    "Save Bitmap: " + fileOutputStream.toString(),
+                    Toast.LENGTH_LONG).show();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,
+                    "Something wrong: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,
+                    "Something wrong: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}
+
