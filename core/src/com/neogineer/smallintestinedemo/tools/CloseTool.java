018a8c20f95b21a4a702532775e90c17b9119c95
Achraf <achraf.amil@gmail.com>
2017-03-23 19:57:59 +0000

    More intuitive CloseTool for Rope.
    Details: CloseTool now checks for OrganPart neighbors (if the touched one is not open).

--- core/src/com/neogineer/smallintestinedemo/tools/CloseTool.java
+++ core/src/com/neogineer/smallintestinedemo/tools/CloseTool.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.physics.box2d.World;
 import com.neogineer.smallintestinedemo.organs.Openable;
 import com.neogineer.smallintestinedemo.organs.OpenableSide;
+import com.neogineer.smallintestinedemo.organs.OrganPart;
+import com.neogineer.smallintestinedemo.organs.rope.RopeOrganPart;
 
 /**
  * Created by neogineer on 25/10/16.
@@ -19,7 +21,27 @@ public class CloseTool extends Tool {
         super.touchDown(screenX, screenY, pointer, button);
 
         if(hitBody!=null){
-            if(hitBody.getUserData() instanceof Openable){
+            OrganPart op = (OrganPart) hitBody.getUserData();
+            if(op instanceof Openable){
+
+                if(op instanceof RopeOrganPart){
+                    RopeOrganPart ropeOp = (RopeOrganPart) op;
+                    try {
+                        ropeOp.getTheOpenOs().close();
+                    }catch (NullPointerException e){
+                        try {
+                            ((RopeOrganPart)ropeOp.getSuturePoints().get(0).getTheOtherOrganPart()).getTheOpenOs().close();
+                        }catch (NullPointerException | ClassCastException ee){
+                            try {
+                                ((RopeOrganPart)ropeOp.getSuturePoints().get(1).getTheOtherOrganPart()).getTheOpenOs().close();
+                            }catch (NullPointerException | ClassCastException eee){
+                                return false;
+                            }
+                        }
+                    }
+                    return true;
+                }
+
                 OpenableSide side = ((Openable) hitBody.getUserData()).getOpenableSide(testPoint.x, testPoint.y);
                 if(side==null)
                     return false;
