ef58dd57f317ee36e7ca88078ce7e8bbb4674050
ladykat <kat@derbsellicon.com>
2017-06-25 15:37:35 +0000

    resetButton
    import phoenixProcess module

---
+++ core/src/com/suturesapp/workspace/tools/ResetTool.java
@@ -0,0 +1,20 @@
+package com.suturesapp.workspace.tools;
+
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by ladykat on 25/06/2017.
+ */
+
+public class ResetTool {
+
+    //OnClickButton Method
+
+    //ProcessPhoenix.triggerRebirth(context);
+
+
+
+}
